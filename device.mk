977787c07846ca3ec55fe0db9787a2476094c377
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-10-06 18:03:23 +0600

    [SQUASH] ginkgo: Remove MIUI camera support
    ginkgo: nuke misys
    
    * its for watermark in miui cam and i nuked it lmao
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    ginkgo: Drop MiuiCamera
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    ginkgo: Drop MiuiCamera overlay
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    Revert "ginkgo: Implement keyhandler for fingerprint shutter"
    
    This reverts commit 2efee61d81726c5cd9317e9a40f4a7a17eebf565.
    
    No meed since we using Google Camera Go.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    ginkgo: depedencies: Remove MIUI camera
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    Revert "ginkgo: keylayout: Redirect keycode 96 to CAMERA"
    
    This reverts commit 4c1ee8129ec721033e668da7766fc63d62cebff4.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    Revert "ginkgo: overlay: Disable 48MP mode in MiuiCamera"
    
    This reverts commit 1214341c05aa7e441546dcbafe794c9f45aa1556.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    
    Co-authored-by: yukiscape <garikapatiamartya@gmail.com>
    Co-authored-by: madmax7896 <thegreatsagar123@gmail.com>
    Co-authored-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- device.mk
+++ device.mk
@@ -14,7 +14,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-miku
 
 PRODUCT_PACKAGES += \
-    GinkgoMiuiCamera \
     NotchBarKiller \
     WillowFrameworkRes
 
@@ -83,9 +82,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
-# Camera
-$(call inherit-product, vendor/miuicamera/config.mk)
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
